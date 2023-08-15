08c617f3512a7737ec441394abe298e21b27255f
munzirulalom <cse.alomb008@gmail.com>
2023-08-15 13:11:47 +0600

    Update: user-search sql

--- ajax/user-search.php
+++ ajax/user-search.php
@@ -1,5 +1,5 @@
 <?php
-if ( empty($_POST['search']) ) {
+if (empty($_POST['search'])) {
 	return;
 }
 
@@ -11,16 +11,19 @@ require_once("db_conn.php");
 
 $table = get_table_name('user');
 
-$stmt = $db->prepare("SELECT * FROM `{$table}` WHERE user_name LIKE '%{$search_term}%' OR user_email LIKE '{$search_term}%'");
-$stmt->execute();
+$stmt = $db->prepare("SELECT * FROM `{$table}` WHERE user_name LIKE :search_term_1 OR user_email LIKE :search_term_2");
+$stmt->execute(array(
+	":search_term_1" => '%' . $search_term . '%',
+	":search_term_2" => $search_term . '%'
+));
 
-while ($user = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+while ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
 	$output .= '<label class="form-selectgroup-item flex-fill">';
-	$output .= '<input type="checkbox" class="form-selectgroup-input" id="'.$user["user_id"].'" onclick="assignedUser('.$user["user_id"].')">';
+	$output .= '<input type="checkbox" class="form-selectgroup-input" id="' . $user["user_id"] . '" onclick="assignedUser(' . $user["user_id"] . ')">';
 	$output .= '<div class="form-selectgroup-label d-flex align-items-center p-3">';
 	$output .= '<div class="me-3"><span class="form-selectgroup-check"></span></div>';
-	$output .= '<div class="form-selectgroup-label-content d-flex align-items-center"><span class="avatar me-3" style="background-image: url('. get_user_image( $user['user_id'] ) .')"></span></div>';
-	$output .= '<div><div class="font-weight-medium">'. $user['user_name'] .'</div><div class="text-muted">'. $user['user_email'] .'</div></div>';
+	$output .= '<div class="form-selectgroup-label-content d-flex align-items-center"><span class="avatar me-3" style="background-image: url(' . get_user_image($user['user_id']) . ')"></span></div>';
+	$output .= '<div><div class="font-weight-medium">' . $user['user_name'] . '</div><div class="text-muted">' . $user['user_email'] . '</div></div>';
 	$output .= '</div>';
 	$output .= '</label>';
 }

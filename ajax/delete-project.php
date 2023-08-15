6236feb94d717af2018d290e66abfed3d34b1c22
munzirulalom <cse.alomb008@gmail.com>
2023-08-15 12:57:16 +0600

    Update: delete-project sql

--- ajax/delete-project.php
+++ ajax/delete-project.php
@@ -1,14 +1,14 @@
 <?php
 require_once("db_conn.php");
 
-if ( empty($_POST['project_id']) OR empty($_POST['status']) ) {
+if (empty($_POST['project_id']) or empty($_POST['status'])) {
 	exit;
 }
 
 $project_id = (int) $_POST['project_id'];
-$status = filter_var( (string)$_POST['status'], FILTER_VALIDATE_BOOLEAN);
+$status = filter_var((string)$_POST['status'], FILTER_VALIDATE_BOOLEAN);
 
-if ( $status != true){
+if ($status != true) {
 	return;
 }
 
@@ -20,26 +20,38 @@ $filetable = get_table_name('attachment');
 
 
 //Delete From Task Catagory Table
-$stmt = $db->prepare("DELETE FROM `{$table}` WHERE cat_id = '{$project_id}'");
-$stmt->execute();
+$stmt = $db->prepare("DELETE FROM `{$table}` WHERE cat_id = :project_id");
+$stmt->execute(array(
+	":project_id" => $project_id
+));
 
 //Delete Sub Task
-$stmt = $db->prepare("SELECT task_id FROM `{$tasktable}` WHERE cat_id = '{$project_id}'");
-$stmt->execute();
-while ($task = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-	$substmt = $db->prepare("DELETE FROM `{$subtasktable}` WHERE task_id = '{$task['task_id']}'");
-	$substmt->execute();
+$stmt = $db->prepare("SELECT task_id FROM `{$tasktable}` WHERE cat_id = :project_id");
+$stmt->execute(array(
+	":project_id" => $project_id
+));
+while ($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
+	$substmt = $db->prepare("DELETE FROM `{$subtasktable}` WHERE task_id = :task");
+	$substmt->execute(array(
+		":task" => $task['task_id']
+	));
 
-	$substmt = $db->prepare("DELETE FROM `{$filetable}` WHERE task_id = '{$task['task_id']}'");
-	$substmt->execute();
+	$substmt = $db->prepare("DELETE FROM `{$filetable}` WHERE task_id = :task");
+	$substmt->execute(array(
+		":task" => $task['task_id']
+	));
 }
 
 //Delete Task
-$stmt = $db->prepare("DELETE FROM `{$tasktable}` WHERE cat_id = '{$project_id}'");
-$stmt->execute();
+$stmt = $db->prepare("DELETE FROM `{$tasktable}` WHERE cat_id = :project_id");
+$stmt->execute(array(
+	":project_id" => $project_id
+));
 
 //Delete From User Task Catagory Table
-$stmt = $db->prepare("DELETE FROM `{$usertable}` WHERE cat_id = '{$project_id}'");
-$stmt->execute();
+$stmt = $db->prepare("DELETE FROM `{$usertable}` WHERE cat_id = :project_id");
+$stmt->execute(array(
+	":project_id" => $project_id
+));
 
 exit;

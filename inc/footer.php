c7f03889da26f170d201824bc36fc05739866a6d
munzirulalom <cse.alomb008@gmail.com>
2023-08-15 14:19:13 +0600

    Update: Footer Source Code link

--- inc/footer.php
+++ inc/footer.php
@@ -13,7 +13,7 @@ require_once("template-parts/_add-project-modal.php");
           <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
               <ul class="list-inline list-inline-dots mb-0">
-                <li class="list-inline-item"><a href="https://github.com/munzirulalom/" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+                <li class="list-inline-item"><a href="https://github.com/munzirulalom/task-management-system" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
               </ul>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
